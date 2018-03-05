fc0c8095a37cce25651ae1a214259b0dc516897e
Erika Campos <erika.campos.design@gmail.com>
2018-03-05 01:38:09 -0300

    [WIP] register cuisine

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,5 +1,19 @@
 class CuisinesController < ApplicationController
+
+  def create
+    cuisine_params = params.require(:cuisine).permit(:name)
+    @cuisine = Cuisine.new(cuisine_params)
+    @cuisine.save
+    redirect_to cuisine_path(@cuisine.id)
+  end
+
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
   def show
     @cuisine = Cuisine.find(params[:id])
   end
+
 end
