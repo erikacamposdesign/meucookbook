dbac876e3fe23fb0d0423b6a62bb6b934e704d69
Erika Campos <erika.campos.design@gmail.com>
2018-03-07 16:49:01 -0300

    visitor_register_recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,8 +4,11 @@ class RecipesController < ApplicationController
     recipe_params = params.require(:recipe).permit(:title, :recipe_type, :cuisine_id,
                     :difficulty, :cook_time, :ingredients, :method)
     @recipe = Recipe.new(recipe_params)
-    @recipe.save
-    redirect_to recipe_path(@recipe.id)
+    if @recipe.save
+      redirect_to recipe_path(@recipe.id)
+    else
+      render :new
+    end
   end
 
   def new
@@ -13,7 +16,6 @@ class RecipesController < ApplicationController
   end
 
   def show
-    id = params[:id]
-    @recipe = Recipe.find(id)
+    @recipe = Recipe.find(params[:id])
   end
 end
